c303bac9aac14ae9b4bfdd864fe55cd7c22acf9d
Hichem Lamraoui <hichem.lamraoui@polymtl.ca>
2020-08-02 13:13:10 -0400

    Mise en place de la composante 'Logout'

--- client/src/app/logout/logout.component.ts
+++ client/src/app/logout/logout.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.scss'],
 })
-export class LogoutComponent implements OnInit {
-  constructor() {
-    /*  */
-  }
+export class LogoutComponent {
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-    /*  */
+  onClick(): void {
+    this.router.navigateByUrl('/login');
   }
 }
