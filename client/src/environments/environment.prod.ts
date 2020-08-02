cd5c9dd92ff3fa0c27611269d6ff9781273a46c8
Hichem Lamraoui <hichem.lamraoui@polymtl.ca>
2020-08-02 01:33:29 -0400

    Ajout de variables dans l'environnement 'Production'

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,234 @@
+// tslint:disable: no-magic-numbers
+
 export const environment = {
   production: true,
+  inputs: {
+    email: {
+      errorMessages: {
+        required: 'You must enter a valid email address.',
+        email: 'Invalid email.',
+      },
+      readonly: true,
+    },
+    password: {
+      errorMessages: {
+        required: 'You must enter a password.',
+        pattern:
+          'Invalid password. It must contain at least 8 characters with at least 1 numeric character, 1 uppercase letter, 1 lowercase letter and 1 special character.',
+      },
+      readonly: true,
+    },
+    confirmPassword: {
+      errorMessages: {
+        required: 'You must confirm the password.',
+        noPassswordMatch: 'Error: password and confirm password do not match.',
+      },
+      readonly: true,
+    },
+    membershipType: {
+      errorMessages: {
+        required: 'You must choose the type of the membership.',
+      },
+      types: ['Buy a house', 'Investment'],
+      readonly: true,
+    },
+    accountType: {
+      errorMessages: {
+        required: 'You must choose the type of the account.',
+      },
+      types: ['Individual', 'Joint'],
+      readonly: true,
+    },
+    firstName: {
+      errorMessages: {
+        required: 'You must enter a first name.',
+      },
+      readonly: true,
+    },
+    lastName: {
+      errorMessages: {
+        required: 'You must enter a last name.',
+      },
+      readonly: true,
+    },
+    birthDay: {
+      errorMessages: {
+        required: 'Invalid date.',
+        belowMinimumDateLimit: 'The minimum date allowed is 01/01/1900.',
+        aboveMaximumDateLimit: 'You must be at least 18 years old!',
+      },
+      acceptedRange: {
+        minDate: '1900-01-01',
+        // User must be at least 18 years old
+        maxDate: new Date(new Date().getFullYear() - 18, new Date().getMonth(), new Date().getDate()).toISOString().slice(0, 10),
+      },
+      readonly: true,
+    },
+    phoneNumber: {
+      errorMessages: {
+        required: 'You must enter a phone number.',
+        pattern: 'Invalid phone number.',
+      },
+      readonly: true,
+    },
+    address: {
+      errorMessages: {
+        required: 'You must enter an address.',
+      },
+      readonly: true,
+    },
+    city: {
+      errorMessages: {
+        required: 'You must enter a city.',
+      },
+      readonly: true,
+    },
+    country: {
+      errorMessages: {
+        required: 'You must choose a country.',
+      },
+      readonly: true,
+    },
+    province: {
+      errorMessages: {
+        required: 'You must choose a province.',
+      },
+      readonly: true,
+    },
+    postalCode: {
+      errorMessages: {
+        required: 'You must enter a valid postal code.',
+        pattern: 'Invalid postal code.',
+      },
+      readonly: true,
+    },
+    socialInsuranceNumber: {
+      errorMessages: {
+        required: 'You must enter a social insurance number (SIN).',
+        pattern: 'Invalid social insurance number (SIN).',
+      },
+      readonly: true,
+    },
+    citizenship: {
+      errorMessages: {
+        required: 'You must choose a citizenship.',
+      },
+      readonly: true,
+    },
+    profession: {
+      errorMessages: {
+        required: 'You must enter a profession.',
+      },
+      readonly: true,
+    },
+    employer: {
+      errorMessages: {
+        required: 'You must enter an employer.',
+      },
+      readonly: true,
+    },
+    employerPhoneNumber: {
+      errorMessages: {
+        required: 'You must enter an employer phone number.',
+        pattern: 'Invalid phone number.',
+      },
+      readonly: true,
+    },
+    numberOfDependents: {
+      errorMessages: {
+        required: 'you must enter the number of your dependents.',
+      },
+      readonly: true,
+    },
+    depositAmount: {
+      errorMessages: {
+        required: 'you must enter your deposit amount.',
+      },
+      readonly: true,
+    },
+    donationForMosque: {
+      errorMessages: {
+        required: 'you must enter your donation amount for mosque.',
+      },
+      readonly: true,
+    },
+    membershipFee: {
+      defaultAmount: 75,
+      readonly: true,
+    },
+    totalAmount: {
+      readonly: true,
+    },
+    relationship: {
+      errorMessages: {
+        required: 'you must enter a relationship.',
+      },
+      types: [
+        'Aunt',
+        'Brother-in-law',
+        'Brother',
+        'Cousin',
+        'Daughter-in-law',
+        'Daughter',
+        'Ex-husband',
+        'Ex-wife',
+        'Father-in-law',
+        'Father',
+        'Fiancé',
+        'Fiancée',
+        'Friend',
+        'Grand-daughter',
+        'Grand-father',
+        'Grand-mother',
+        'Grandson',
+        'Great grand-father',
+        'Great grand-mother',
+        'Half-brother',
+        'Half-sister',
+        'Husband',
+        'Mother in-law',
+        'Mother',
+        'Nephew',
+        'Niece',
+        'Sister in-law',
+        'Sister',
+        'Son-in-law',
+        'Son',
+        'Step-daughter',
+        'Step-father',
+        'Step-mother',
+        'Step-son',
+        'Uncle',
+        'Wife',
+      ],
+      readonly: true,
+    },
+    userAgreement: {
+      errorMessages: {
+        required: 'you must accept the regulation.',
+      },
+      text: {
+        individual: 'I have read the Regulations and By-laws of the "Co-operative" and I fully agree to abide by them.',
+        joint: 'We have read the Regulations and By-laws of the "Co-operative" and We fully agree to abide by them.',
+      },
+      readonly: true,
+    },
+  },
+  registration: {
+    batchLength: 8,
+  },
+  userProfil: {
+    tabs: {
+      loginTab: 0,
+      personalTab: 1,
+      jointMemberTab: 2,
+    },
+    forms: {
+      registrationFormStep1: 0,
+      registrationFormStep2: 1,
+      registrationFormStep3: 2,
+      registrationFormStep4: 3,
+    },
+    snackbarDuration: 3000,
+  },
 };
